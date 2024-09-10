f998bcd460408cc5f0c68905df48b8d8109db0a1
mrchuw <jgabrielmonteiro@hotmail.com>
2024-09-10 13:56:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -58,5 +58,5 @@ register_tortoise(app, db_url="sqlite://db.sqlite3", modules={"models": ["schema
                   add_exception_handlers=True)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=5454, log_level="info", reload=True)
-    # uvicorn.run("main:app", host="0.0.0.0", port=5454, log_level="info", workers=4)
+    # uvicorn.run("main:app", host="0.0.0.0", port=5454, log_level="info", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5454, log_level="info", workers=4)
